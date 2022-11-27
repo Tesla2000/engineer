128c782e44e938f6a421b5b7a4468c00c3fe583f
Tesla2000 <fratajczak124@gmail.com>
2022-11-27 18:11:08 +0100

    removed concentration, changed way of saving files

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import os
+from enum import Enum
 from time import sleep
 
 import pygame
@@ -11,7 +12,6 @@ bird_images = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
-
 
 class Bird:
     def __init__(self, y):
@@ -29,16 +29,27 @@ class Bird:
         self.image = self.images.pop(0)
         self.images.append(self.image)
         self.y = (self.y + time * self.speed) % WINDOW_HEIGHT
-        self.tilt += (-1)**self.changes*self.rotation_velocity*time
+        self.tilt += (-1) ** self.changes * self.rotation_velocity * time
         if abs(self.tilt) > self.max_tilt:
             self.changes += 1
         rotated_image = pygame.transform.rotate(self.image, self.tilt)
-        new_rect = rotated_image.get_rect(center=self.image.get_rect(topleft=(WINDOW_WIDTH // 2, self.y)).center)
+        new_rect = rotated_image.get_rect(center=self.image.get_rect(topleft=(WINDOW_WIDTH // 2 - 20, self.y)).center)
         window.blit(rotated_image, new_rect.topleft)
 
 
-def display_concentration(window, bird, time):
+class Prompt:
+    def __init__(self, image):
+        self.image = pygame.transform.scale2x(pygame.image.load(image))
+        self.counter = 0
+
+    def move(self, window):
+        window.blit(self.image, self.image.get_rect(center=self.image.get_rect(topleft=(0, 0)).center))
+
+
+def display_concentration(window, bird, time, prompt=None):
     window.fill(black)
+    if prompt is not None:
+        prompt.move(window)
     bird.move(window, time)
     sleep(time)
     pygame.display.update()
--- main.py
+++ main.py
@@ -4,18 +4,22 @@ import threading
 import time
 from datetime import datetime
 
+import cv2
 import numpy as np
 import pygame
-from numpy.fft import fft, fftfreq
 from pylsl import resolve_stream, StreamInlet
 
+from connection import Stream, take_data_amplitude, take_data_frequency
 from filters import bandpass, notch
-from connection import filter_sample, Stream, take_data_amplitude, take_data_frequency
-import cv2
-from gui import Bird, WINDOW_HEIGHT, WINDOW_WIDTH, display_concentration
+from gui import Bird, WINDOW_HEIGHT, WINDOW_WIDTH, display_concentration, Prompt
 
 sampling_rate = 250
 electricity_frequency = 50  # Europe 50 USA 60
+
+activity_numbers = {'break': 0, 'imaginary_left': 1, 'imaginary_right': 2, 'silent_yes': 3, 'silent_no': 4,
+                    'imaginary_yes': 3, 'imaginary_no': 4}
+numbers_activity = {0: 'break', 1: 'imaginary_left', 2: 'imaginary_right', 3: 'silent_yes', 4: 'silent_no',
+                    5: 'imaginary_yes', 6: 'imaginary_no'}
 
 
 def examine_concentration(session, length_of_preparations_session=30, length_concentration_session=120,
@@ -33,100 +37,69 @@ def examine_concentration(session, length_of_preparations_session=30, length_con
     collector = frequency_collector(f'data/{session}/test_filtered.csv', f'data/{session}/test_unfiltered.csv',
                                     notch(electricity_frequency, sampling_rate), bandpass(5, 35, sampling_rate))
     while time.time() - length_of_preparations_session < start:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
         display_concentration(window, bird, 0.01)
         next(collector)
-    full_power = np.sum(np.loadtxt(f'data/{session}/test_filtered.csv', usecols=range(1, n_channels + 1)))
+    full_power = np.sum(
+        np.loadtxt(f'data/{session}/test_filtered.csv', usecols=range(1, n_channels + 1))[:max_frequency])
     alpha_power = np.loadtxt(f'data/{session}/test_filtered.csv',
                              usecols=range(1, n_channels + 1))[:, alpha_frequency[0]:alpha_frequency[1] + 1]
     beta_power = np.loadtxt(f'data/{session}/test_filtered.csv',
                             usecols=range(1, n_channels + 1))[:, beta_frequency[0]:beta_frequency[1] + 1]
+    collector = frequency_collector(f'data/{session}/concentration_filtered.csv',
+                                    f'data/{session}/concentration_unfiltered.csv',
+                                    notch(electricity_frequency, sampling_rate), bandpass(5, 35, sampling_rate))
     relative_alpha, relative_beta = alpha_power / full_power, beta_power / full_power
     while time.time() - (length_of_preparations_session + length_concentration_session) < start:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
         filtered, unfiltered = next(collector)
         bird.speed = 100 / (
-                    (np.sum(filtered[beta_frequency[0]:beta_frequency[1] + 1]) / np.sum(filtered)) / relative_beta)
-        display_concentration(window, bird, 0.01)
+                (np.sum(filtered[beta_frequency[0]:beta_frequency[1] + 1]) / np.sum(filtered)) / relative_beta)
+        display_concentration(window, bird, 0.01, Prompt('imgs/concentrate.png'))
+    collector = frequency_collector(f'data/{session}/relax_filtered.csv', f'data/{session}/relax_unfiltered.csv',
+                                    notch(electricity_frequency, sampling_rate), bandpass(5, 35, sampling_rate))
     while time.time() - (length_of_preparations_session + length_concentration_session + length_relax_session) < start:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
         filtered, unfiltered = next(collector)
         bird.speed = 100 / (
-                    (np.sum(filtered[alpha_frequency[0]:alpha_frequency[1] + 1]) / np.sum(filtered)) / relative_alpha)
-        display_concentration(window, bird, 0.01)
+                (np.sum(filtered[alpha_frequency[0]:alpha_frequency[1] + 1]) / np.sum(filtered)) / relative_alpha)
+        display_concentration(window, bird, 0.01, Prompt('imgs/relax.png'))
+    pygame.quit()
 
 
-def show_image(image_path, display_duration=4000, break_duration=2000):
+def show_image(image_path, display_duration=4000):
     cv2.imshow("window", cv2.imread(image_path))
     cv2.waitKey(display_duration)
+
+
+def pause(duration=2000):
     cv2.imshow("window", cv2.imread('imgs/empty.png'))
-    cv2.waitKey(break_duration)
+    cv2.waitKey(duration)
 
 
-def examine_MI(session, wait_before_display=2000, display_time=2000, break_time=2000, trials=60):
-    for _ in range(trials):
-        showing = threading.Thread(target=show_image, args=('imgs/' + (choice := random.choice(('l', 'r'))) +
-                                                            '_letter.png', display_time + wait_before_display
-                                                            , break_time))
-        collecting = threading.Thread(target=collect_amplitude, args=
-        (f'data/{session}/MI_' + ('right' if choice == 'r' else 'left') + '_hand.csv'
-         , wait_before_display, display_time, break_time))
-        showing.start()
-        collecting.start()
-        showing.join()
-        collecting.join()
-
-
-def examine_SS(session, wait_before_display=2000, display_time=2000, break_time=2000, trials=60):
-    for _ in range(trials):
-        showing = threading.Thread(target=show_image, args=('imgs/' + (choice := random.choice(('t', 'n'))) +
-                                                            '_letter.png', display_time + wait_before_display
-                                                            , break_time))
-        collecting = threading.Thread(target=collect_amplitude, args=
-        (f'data/{session}/SS_' + ('no' if choice == 'n' else 'yes') + '.csv'
-         , wait_before_display, display_time, break_time))
-        showing.start()
-        collecting.start()
-        showing.join()
-        collecting.join()
-
-
-def examine_IS(session, wait_before_display=2000, display_time=2000, break_time=2000, trials=60):
-    for _ in range(trials):
-        showing = threading.Thread(target=show_image, args=('imgs/' + (choice := random.choice(('t', 'n'))) +
-                                                            '_letter.png', display_time + wait_before_display
-                                                            , break_time))
-        collecting = threading.Thread(target=collect_amplitude, args=
-        (f'data/{session}/IS_' + ('no' if choice == 'n' else 'yes') + '.csv'
-         , wait_before_display, display_time, break_time))
-        showing.start()
-        collecting.start()
-        showing.join()
-        collecting.join()
-
-
-def collect_amplitude(write_to, wait_before_display=2000, display_time=2000, break_time=2000):
-    start = time.time()
+def examine_paradigm(session, amplitude_stream, choices, wait_before_register=2, display_time=2, break_time=2, trials=60):
     n_channels = 8
-    streams = resolve_stream('type', 'EEG')
-    amplitude_stream = StreamInlet(streams[Stream.AMPLITUDE])
-    while time.time() - wait_before_display / 1000 > start:
-        take_data_amplitude(amplitude_stream, n_channels)
-    while time.time() - (wait_before_display + display_time) / 1000 > start:
-        with open(write_to, 'a') as file:
-            timestamp, samples = take_data_amplitude(amplitude_stream, n_channels)
-            file.write(','.join((str(timestamp), *map(str, samples))))
-    while time.time() - (wait_before_display + display_time + break_time) / 1000 > start:
-        take_data_amplitude(amplitude_stream, n_channels)
+    for _ in range(trials):
+        showing = threading.Thread(target=pause, args=(1000 * break_time,))
+        showing.start()
+        start = time.time()
+        while break_time + start < time.time():
+            with open(session, 'a') as file:
+                timestamp, data = take_data_amplitude(amplitude_stream, n_channels)
+                file.write(','.join((timestamp, 0, *data)) + '\n')
+        showing.join()
+        showing = threading.Thread(target=show_image, args=('imgs/' + numbers_activity[choice := random.choice(choices)],
+                                                            1000 * break_time))
+        start = time.time()
+        while wait_before_register + start < time.time():
+            with open(session, 'a') as file:
+                timestamp, data = take_data_amplitude(amplitude_stream, n_channels)
+                file.write(','.join((timestamp, 0, *data)) + '\n')
+        showing.join()
+        showing.start()
+        start = time.time()
+        while display_time + start < time.time():
+            with open(session, 'a') as file:
+                timestamp, data = take_data_amplitude(amplitude_stream, n_channels)
+                file.write(','.join((timestamp, choice, *data)) + '\n')
+        showing.join()
 
 
 def frequency_collector(write_to_filtered, write_to_unfiltered, *filters, duration=120):
@@ -145,15 +118,21 @@ def frequency_collector(write_to_filtered, write_to_unfiltered, *filters, durati
 
 def start_session():
     now = str(datetime.now()).split('.')[0]
-    session_name = f'data/session_{now}'
-    os.makedirs(session_name)
-    for paradigm in ('MI_right_hand', 'MI_left_hand', 'SS_yes', 'SS_no', 'IS_yes', 'IS_no', 'concentration_filtered',
-                     'relax_filtered', 'concentration_unfiltered', 'relax_unfiltered', 'test_filtered',
-                     'test_unfiltered'):
-        with open(f'data/session_{now}/{paradigm}.csv', 'w') as _:
-            pass
+    session_name = f'data/session_{now}.csv'
+    with open(session_name, 'w') as _:
+        pass
     return session_name
 
 
+def main():
+    session = start_session()
+    stream = resolve_stream('type', 'EEG')
+    amplitude_stream = StreamInlet(stream[Stream.AMPLITUDE])
+    examine_paradigm(session, amplitude_stream, (1, 2,))
+    examine_paradigm(session, amplitude_stream, (3, 4))
+    examine_paradigm(session, amplitude_stream, (5, 6))
+    # examine_concentration(session)
+
+
 if __name__ == '__main__':
-    examine_MI(start_session())
+    main()
